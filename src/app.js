e07d72761a00a2ecfefefb7dce3d03144af367f1
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-08 15:45:10 +0600

    Duplicate

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var towSum = function (nums, target) {
+var containDuplicate = function (nums) {
+    let set = new Set();
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        if (set.has(nums[i])) {
+            return true;
         }
+        set.add(nums[i]);
     }
-    return [];
+    return false;
 };
-const nums = [2, 7, 11, 15];
-console.log(towSum(nums, 9));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-var towSum = function (nums: number[], target: number): number[] {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-  return [];
-};
+var containDuplicate = function (nums: number[]): boolean {
+  let set = new Set();
 
-const nums = [2, 7, 11, 15];
-console.log(towSum(nums, 9));
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+    set.add(nums[i]);
+  }
+  return false;
+};
