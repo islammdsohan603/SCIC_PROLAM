9b421fda0cc474a6f9adf4aa9e93e6baf7374f73
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-08 14:44:29 +0600

    Ransom Note

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 
-var isAnagram = function (s, t) {
 
-  for (let i = 0; i < s.length; i++) {
-    if (t.includes(s[i])) {
-      t = t.replace(s[i], '');
-    } else {
+var canConstruct = function (ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (magazine.includes(ransomNote[i])) {
+      magazine = magazine.replace(ransomNote[i], '');
+    }
+    else {
       return false;
     }
-  }
-  return t.length === 0;
 
-};
+
+  }
+
+  return true;
+}
